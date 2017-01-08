8b8118a3ce18aec0d3daddeb8896717208617cc3
agben <ben@Delli7.home>
2017-01-08 22:14:54 +0000

    v0.1.5.4 re-instated identification of links between functions and object size checks

--- ce_scan_obj.c
+++ ce_scan_obj.c
@@ -2,43 +2,46 @@
 //
 // Scan object files for useful info to add to the CE program database
 //
-//	usage:	ce_scan_obj program.ce
-//		where arg1 is the name of the file containing the objdump analysis of the object file
+//	usage:	objdump -rtl program.o >> program.ce
+//			ce_scan_obj program.ce
+//		objdump analyses the object file and identifies symbols, global variables, functions, etc... used
 //
 //	GNU GPLv3+ licence	CE - Coding Ecosystem by Andrew Bennington 2015 <www.benningtons.net>
 //
 //--------------------------------------------------------------
 
-#include <fcntl.h>
+#include <ctype.h>		// for toupper
+#include <fcntl.h>		// file file access functions
 #include <getopt.h>		// for getopt_long - argument parsing
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h>		// for  strtol
+#include <string.h>		// for memcmp, strncmp
 
-#include <fa_def.h>
-#include <ce_main.h>
-#include <ut_date.h>
-#include <ut_error.h>
+#include <fa_def.h>		// gxt file access functions
+#include <ce_main.h>	// clice global data
+#include <ut_date.h>	// gxt date functions
+#include <ut_error.h>	// error and debug macros
 
-#define	BUFF_S0 80
+#define	BUFF_S0 80				// max objdump line width read from file
+#define	CE_SCAN_MODULES_M0 50	// max modules one program can link to
 
 int main(int argc, char **argv)
   {
-	FILE *fp;
-	char sBuff[BUFF_S0];			// input buffer for init file reads
-	int i, j;
-	int iSection = 0;				// section count through objdump 0=start, 1=Symbols,  2=.text, 3=remainder
-//	int iSize = 0;					// size of object file
-//	char *cp;
+	FILE *fp;					// file pointer
+	char sBuff[BUFF_S0];		// input buffer for file reads
+	int i, j, k;				// general purpose
+	int iSection = 0;			// section count through objdump 0=start, 1=Symbols,  2=.text, 3=remainder
+
+	char sModule[CE_SCAN_MODULES_M0][CE_NAME_S0];	// temp list of module names found in symbols table
+	int	iModule = 0;								// Count of how many found
 
 	spCE = (struct CE_FIELDS*) &CE;
 	spCEL = (struct CEL_FIELDS*) &CEL;
+
 						// valid arguments: name, has_arg(yes=1, no=0, opt=2), flag, val
 	static struct option long_options[] = {
 		{"help",	0,	0,		0},	// 0	Keep this order for parsing after getopt_long
 		{"version",	0,	0,		0},	// 1
-		{NULL,		0,	NULL,	0}
-	};
+		{NULL,		0,	NULL,	0}};
 
 	int option_index = 0;
 
@@ -49,35 +52,34 @@ int main(int argc, char **argv)
 							long_options,			//permitted long arguments   i.e. --version
 							&option_index)) != -1)
 	  {
-		if (i == '?' || i != 0)					// invalid arg or arg qualifier so abort
-		  {
+		if (i == '?' || i != 0)				// invalid arg or arg qualifier so abort
 			return -1;
-		  }
-		else if (option_index == 0)				// --help
+		else if (option_index == 0)			// --help
 		  {
 			ce_help();
 			return 0;
 		  }
-		else if (option_index == 1)				// --version
+		else if (option_index == 1)			// --version
 		  {
 			ce_version();
 			return 0;
 		  }
 	  }
 
-	ut_check(optind == argc-1,					// still an argument remaining? the object to scan
-			"Usage = ce_scan_obj <object.ce>"); // #TODO rather than create a file and pass its name we could read from stdin?
+	ut_check(optind == argc-1,						// still an argument remaining? the object to scan
+		"Usage = ce_scan_obj <object.ce>"); 		// #TODO rather than create a file and pass its name we could read from stdin?
 
 	ut_check(cef_main(FA_INIT+FA_OPEN, 0) == 0,
-			"failed to open clice db");			// initialise libgxtfa and open clice db
+			"failed to open clice db");				// initialise libgxtfa and open clice db
 
-	ut_date_now();								// get current date & time
+	ut_date_now();									// get current date & time
 
-	strncpy(spCEL->sName, argv[1], CE_NAME_S0);	// Calling program name
-	sprintf(&sBuff[0], "%s.ce", spCEL->sName);	// object filename to be scanned
+//	strncpy(spCEL->sName, argv[1], CE_NAME_S0);		// Calling program name
+//	sprintf(&sBuff[0], "%s.ce", spCEL->sName);		// object filename to be scanned
+	sprintf(&sBuff[0], "%s.ce", argv[1]);			// objdump filename to be scanned
 
-	fp = fopen(&sBuff[0], "r");					// Open file as read-only
-	ut_check(fp != NULL, "open obj");			// jumps to error: if not ok
+	fp = fopen(&sBuff[0], "r");						// Open file as read-only
+	ut_check(fp != NULL, "open obj");				// jumps to error: if not ok
 
 	while (fgets(sBuff, BUFF_S0, fp) != NULL)
 	  {
@@ -87,58 +89,86 @@ int main(int argc, char **argv)
 		  {
 			if (memcmp(sBuff, "SYMBOL TABLE:", 13) == 0) iSection++;
 		  }
-		else if (iSection == 1)					// Symbol section
+		else if (iSection == 1)						// Symbol section
 		  {
 			if (memcmp(sBuff, "RELOCATION RECORDS FOR [.text]:", 31) == 0)
-			  {
 				iSection++;
-			  }
-			else if (sBuff[0] == '0' && sBuff[23] == 'f')	// find source filename
+			else if (sBuff[0] == '0' &&
+					 sBuff[23] == 'f')				// find source filename
 			  {
-				j=48;
-				for (i=0; i < CE_SOURCE_S0 && sBuff[j] != '\n'; i++)	// sBuff will have a line feed before it terminating null
-						spCE->sSource[i]=sBuff[j++];
-				for ( ; i < CE_SOURCE_S0; i++) spCE->sSource[i]='\0';	// null fill remainder of string
+				CE.cLang='?';
+				j=48;								// start position in objdump output of full source filename
+				k=0;								// flag to spot file extension
+				for (i=0; i < CE_SOURCE_S0 &&
+							sBuff[j] != '\n'; i++)	// sBuff will have a line feed before it terminating null
+				  {
+					if (k == 0 && sBuff[j] == '.')
+						k++;
+					else if (k == 1 && sBuff[j] > ' ')
+					  {
+						CE.cLang=toupper(sBuff[j]);	// use 1st digit of file extension to show language used
+						k++;
+					  }
+
+					CE.sSource[i]=sBuff[j++];
+				  }
+				for ( ; i < CE_SOURCE_S0; i++)
+					CE.sSource[i]='\0';				// null fill remainder of string
+
+				if (CE.cLang == '?')
+					printf("CE: No valid file extension found\n");
 			  }
-			else if (sBuff[0] == '0' && sBuff[23] == 'F')	// find module name(s)
+			else if (sBuff[0] == '0' &&
+					 sBuff[23] == 'F')				// find module name(s)
 			  {
 				j=48;
 				if (memcmp(&sBuff[j], "main", 4) == 0)		// replace 'main' modules with the source filename
-				  {
-					for (i = 0; i < CE_NAME_S0 && spCE->sSource[i] != '\0' && spCE->sSource[i] != '.'; i++)
-						spCE->sName[i]=spCE->sSource[i];
-				  }
+					for (i = 0; i < CE_NAME_S0 &&
+								CE.sSource[i] != '\0' &&
+								CE.sSource[i] != '.'; i++)
+						CE.sName[i]=CE.sSource[i];
 				else
-				  {
-					for (i=0; i < CE_NAME_S0 && sBuff[j] != '\n'; i++)	// sBuff will have a line feed before it terminating null
-						spCE->sName[i]=sBuff[j++];
-				  }
-				for ( ; i < CE_NAME_S0; i++) spCE->sName[i]='\0';	// null fill remainder of string
-				printf("Source=%s  Module=%s\n", spCE->sSource, spCE->sName);
+					for (i=0; i < CE_NAME_S0 && sBuff[j] != '\n'; i++)	// sBuff will have a line feed before a terminating null
+						CE.sName[i]=sBuff[j++];
+				for ( ; i < CE_NAME_S0; i++) CE.sName[i]='\0';	// null fill remainder of string
 
-				spCE->iCDate=gxt_iDate[0];						// Update last compiled date and time
-				spCE->iCTime=gxt_iTime[0];
+				i=strtol(&sBuff[31], NULL, 16);					// Extract module size from hex to dec
+				CE.iCDate=gxt_iDate[0];							// Update last compiled date and time
+				CE.iCTime=gxt_iTime[0];
 
-				CE.bmField=CEF_ID_B0;							// Select which fields to read
+				CE.bmField=CEF_ID_B0+CEF_SIZE_B0;				// Select which fields to read
 				CEL.bmField=0;
 				if (cef_main(FA_READ+FA_KEY1+FA_STEP, 0) == FA_OK_IV0)	// Check if this module exists in clice db?
 				  {												// #TODO add warning if module name found in new source file
+					if (CE.iSize > 0)
+					  {
+
+						j=((i-CE.iSize)*100)/CE.iSize;			// workout % change in object size
+						if (j > 0)
+							printf("CE: Ooops! %s grown by %d%%\n",
+									CE.sName, j);
+						else if (j < 0)
+							printf("CE: Hurrah! %s shrunk by %d%%\n",
+									CE.sName, 0-j);
+					  }
+					CE.iSize=i;
+
 					i=FA_UPDATE+FA_KEY0;						// Yes so update with current time marker
-					CE.bmField=CEF_SOURCE_B0+CEF_LAST_MOD_B0;	// Keep source filename updated
+					CE.bmField=CEF_SOURCE_B0+CEF_LANG_B0+
+								CEF_SIZE_B0+CEF_LAST_COMP_B0;	// update the fields we can
 				  }
 				else
 				  {
-					printf("CE: New program module added - %s\n", spCE->sName);
-					spCE->iType=CE_PRG_T0;
-					spCE->iStatus=0;
-					spCE->iMDate=spCE->iCDate;
-					spCE->iMTime=spCE->iCTime;
-					sprintf(spCE->sDesc, "Need to scan source for description");
-					spCE->iSize=0;
-					ut_check(getcwd(spCE->sDir,
-								sizeof(spCE->sDir)),
-								"getcwd");						// get current working directory
-					spCE->cLang='C';							// #TODO need to work out the source language from the object dump
+					printf("CE: New program module added - %s\n", CE.sName);
+					CE.iType=CE_PRG_T0;
+					CE.iStatus=0;
+					CE.iSize=i;
+					CE.iMDate=CE.iCDate;
+					CE.iMTime=CE.iCTime;
+					sprintf(CE.sDesc, "Need to scan source for description");
+					ut_check(getcwd(CE.sDir,
+								sizeof(CE.sDir)),
+								"getcwd");				// get current working directory
 
 					i=FA_WRITE;							// Insert all columns into db
 					CE.bmField=FA_ALL_COLS_B0;
@@ -146,69 +176,76 @@ int main(int argc, char **argv)
 				ut_check(cef_main(i, 0) == FA_OK_IV0, "update CE");
 
 			  }
+			else if (sBuff[0] == '0' && sBuff[23] == ' ' &&
+				memcmp(&sBuff[25], "*UND*", 5) == 0)	// find module name(s)
+			  {
+				j=48;									// start position in objdump output of called module name
+				for (i=0; i < CE_NAME_S0 &&
+							sBuff[j] != '\n'; i++)		// sBuff will have a line feed before it terminating null
+					sModule[iModule][i]=sBuff[j++];
+				for ( ; i < CE_NAME_S0; i++)
+					sModule[iModule][i]='\0';			// null fill remainder of string
+				iModule++;
+			  }
 		  }
-		else if (iSection == 2)					//  .text relocation records
+		else if (iSection == 2)							//  .text relocation records
 		  {
 			if (sBuff[0] == 0)
+				break;									// blank line marks the end of the section
+			else if (memcmp(sBuff, "OFFSET", 6) == 0)
+				continue;								// skip column headers
+			else if (sBuff[0] == '0' && sBuff[35] != '.')
 			  {
-				break;							// blank line marks the end of the section
+				j=35;									// start position in objdump output of called module name
+				for (i=0; i < CE_NAME_S0 &&
+							sBuff[j] != '\n' &&
+							sBuff[j] != '+' &&
+							sBuff[j] != '-'; i++)		// sBuff will have a line feed before it terminating null
+					CEL.sCalls[i]=sBuff[j++];
+				for ( ; i < CE_NAME_S0; i++)
+					CEL.sCalls[i]='\0';					// null fill remainder of string
+
+				for (i=0; i < iModule &&
+						strncmp(CEL.sCalls, sModule[i], CE_NAME_S0) != 0;
+						i++);
+				if (i < iModule)						// matched a function call with one listed in the symbols table
+				  {
+					CE.bmField=0;						// Select which fields to read
+					CEL.bmField=CEF_LINK_ID_B0;			// Just get the ID to confirm if row already exists
+					if (cef_main(FA_READ+FA_STEP,
+						"cl.name = % AND cl.calls = %") == FA_OK_IV0)	// Link between these modules already exists?
+					  {
+						i=FA_UPDATE+FA_KEY2;			// Yes so update with current time marker
+						CEL.bmField=CEF_LINK_TIME_B0;	// Only need to update the time stamp
+					  }
+					else
+					  {
+						printf("CE: %s now using %s\n",
+								CEL.sName, CEL.sCalls);	// No? a new link has been established
+						sprintf(CEL.sCode, "no code extract");
+						CEL.cRel=CEL_REL_UNDEF_V0;
+						i=FA_WRITE;						// Insert into db
+						CEL.bmField=FA_ALL_COLS_B0;
+					  }
+					CEL.iTime=gxt_iTime[0];				// Mark with current time
+					ut_check(cef_main(i, 0) == FA_OK_IV0, "update CEL");
+				  }
 			  }
-			else
+			else if (sBuff[0] > 'A' && sBuff[0] < 'z')	// module name found
 			  {
-				continue;
+				if (memcmp(sBuff, "main(", 5) == 0)		// replace 'main' modules with the source filename
+					for (i = 0; i < CE_NAME_S0 && CE.sSource[i] != '\0' && CE.sSource[i] != '.'; i++)
+						CEL.sName[i]=CE.sSource[i];
+				else
+					for (i=0; i < CE_NAME_S0 &&
+								sBuff[i] != '('; i++)	// update current module name
+						CEL.sName[i]=sBuff[i];
+				for ( ; i < CE_NAME_S0; i++) CEL.sName[i]='\0';		// null fill remainder of string
 			  }
 		  }
 	  }
 
-
-/*
-		i=0;
-		while (sBuff[i] > ' ')					// unpack referenced module name
-		  {
-			spCEL->sCalls[i]=sBuff[i];
-			i++;
-		  }
-		spCEL->sCalls[i]=0;
-		ut_debug("name=%s", spCEL->sCalls);
-
-		spCEL->cRel=sBuff[++i];					// unpack reference type
-		ut_debug("rel=%c", spCEL->cRel);
-
-		if (spCEL->cRel == CEL_REL_UNDAT_V0 ||		// variables and structs - #TODO ignored for now.
-			spCEL->cRel == CEL_REL_GDATA_V0 ||
-			spCEL->cRel == CEL_REL_LDATA_V0)
-			continue;
-		else if (spCEL->cRel == CEL_REL_GTEXT_V0)	// symbol for this module (or one of several in this source file)
-		  {
-			sBuff[i+18]='\0';					// Null terminate for atoi
-			cp=&sBuff[i+11];
-			iSize+=atoi(cp);					// module size
-		  }
-		else if (spCEL->cRel == CEL_REL_UNDEF_V0)	// undefined item - likely a link to another clice module or system routine
-		  {
-			CE.bmField=0;						// Select which fields to read
-			CEL.bmField=CEF_LINK_ID_B0;			// Just get the ID to confirm if row already exists
-			if (cef_main(FA_READ+FA_STEP,
-	    		"cl.name = % AND cl.calls = %") == FA_OK_IV0)	// Link between these modules already exists?
-			  {
-				i=FA_UPDATE+FA_KEY2;			// Yes so update with current time marker
-				CEL.bmField=CEF_LINK_TIME_B0;	// Only need to update the time stamp
-			  }
-			else
-			  {
-				printf("CE: now using %s\n", spCEL->sCalls);	// No? a new link has been established
-				i=FA_WRITE;										// Insert into db
-				CEL.bmField=FA_ALL_COLS_B0;
-			  }
-			spCEL->iTime=gxt_iTime[0];				// Mark with current time
-			ut_check(cef_main(i, 0) == FA_OK_IV0, "update CEL");
-		  }
-		else
-			ut_error("type: %c", spCEL->cRel);
-	  }
-
-
-	spCEL->iTime=gxt_iTime[0];				// Check for unused (not time stamped) links
+	CEL.iTime=gxt_iTime[0];					// Check for unused (not time stamped) links
 	CE.bmField=0;							// Select which fields to read
 	CEL.bmField=CEF_LINK_CALLS_B0;
 	ut_check(cef_main(FA_READ, "cl.name = % AND cl.time <> %") == FA_OK_IV0,
@@ -216,46 +253,19 @@ int main(int argc, char **argv)
 	i=0;
 	while (cef_main(FA_STEP,0) == FA_OK_IV0)
 	  {
-		printf("CE: no longer using %s\n", spCEL->sCalls);
+		printf("CE: no longer using %s\n", CEL.sCalls);
 		i=1;
 	  }
 
 	if (i == 1)
 	  {
-		spCEL->iTime=gxt_iTime[0];			// Purge all remaining unused (not time stamped) links
+		CEL.iTime=gxt_iTime[0];				// Purge all remaining unused (not time stamped) links
 		CE.bmField=0;						// Select which table to delete from
 		CEL.bmField=FA_ALL_COLS_B0;
 		ut_check(cef_main(	FA_DELETE,
 							"cl.name = % AND cl.time <> %") == FA_OK_IV0,
 			"delete CEL");						// jumps to error: if not ok
 	  }
-
-	strncpy(spCE->sName, spCEL->sName, CE_NAME_S0);		// Now read main module and update details
-	CE.bmField=CEF_ID_B0+CEF_SIZE_B0;			// Select which fields to read
-	CEL.bmField=0;
-	ut_check(cef_main(FA_READ+FA_STEP,
-					"ce.name = %") == FA_OK_IV0,	// read using name as key
-		"read CE");									// jumps to error: if not ok
-
-	if (spCE->iSize > 0)
-	  {
-		i=((iSize-spCE->iSize)*100)/spCE->iSize;		// workout % change in object size
-		if (i > 0)
-			printf("CE: Ooops grown by %d%%\n", i);
-		else if (i < 0)
-			printf("CE: Hurrah shrunk by %d%%\n", 0-i);
-	  }
-	ut_debug("Previous size:%d new size:%d", spCE->iSize, iSize);
-
-	spCE->iSize=iSize;
-	spCE->iCDate=gxt_iDate[0];					// Record date & time of last compilation
-	spCE->iCTime=gxt_iTime[0];
-
-	CE.bmField=CEF_LAST_COMP_B0+CEF_SIZE_B0;	// Only need to update size & last compiled date & time
-	CEL.bmField=0;
-	ut_check(cef_main(FA_UPDATE, "ce.id = %") == FA_OK_IV0,		// update using id as key
-		"update CE");											// jumps to error: if not ok
-*/
 
 error:
 	if (fp != NULL) fclose(fp);
